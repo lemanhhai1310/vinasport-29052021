78c8b0ba8b5823e0291675da5c5711eaf679f833
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-05-30 01:35:12 +0700

    home

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section-small home__conTent">
+    <div class="uk-container">
+        <div class="home__bannerTop uk-margin">
+            <a href=""><img src="images/Group503.png" alt=""></a>
+        </div>
+        <div class="uk-grid-small uk-grid-55-m" uk-grid>
+            <div class="uk-width-expand">
+                <div class="uk-grid-small uk-grid-34-m" uk-grid>
+                    <div class="uk-width-expand">
+                        <div class="uk-cover-container home__box1">
+                            <img src="images/man-utd-san-co-phat-hien-moi-ma-cu-thay-the-sancho-102650.png" alt="" uk-cover>
+                            <canvas width="700" height="513"></canvas>
+                            <div class="uk-padding-small uk-flex uk-flex-bottom uk-position-bottom home__box1__content">
+                                <h4 class="uk-h4 uk-margin-remove home__box1__content__title"><a href="">Man Utd sẵn có phát hiện mới mà cũ thay thế Sancho</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-1-3@m">
 
+                    </div>
+                </div>
+            </div>
+            <div class="uk-width-1-3@m">
+
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
