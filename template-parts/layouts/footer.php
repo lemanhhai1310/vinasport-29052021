c1010dc4c7c1b2a59e02ae5c5521ecc35681b135
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-05-29 17:13:45 +0700

    footer
    style footer

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,70 @@
+<footer id="footer" class="footer uk-section-small uk-background-01 uk-light">
+    <div class="uk-container">
+        <div uk-grid>
+            <div class="uk-width-auto">
+                <img src="images/logo_f.png" alt="">
+            </div>
+            <div class="uk-width-expand">
+
+            </div>
+        </div>
+        <div class="uk-child-width-1-3@m" uk-grid>
+            <div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Cơ quan quản lý trang thông tin điện tử :</h5>
+                    <div class="footer__txt">TỔNG CÔNG TY TRUYỀN THÔNG VNPT-MEDIA</div>
+                </div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Cơ quan chủ quản :</h5>
+                    <div class="footer__txt">Tập đoàn Bưu chính Viễn thông Việt Nam (VNPT)</div>
+                </div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Chịu trách nhiệm quản lý nội dung:</h5>
+                    <div class="footer__txt">Ông Nguyễn Văn Tấn - Phó TGĐ VNPT Media</div>
+                </div>
+                <div class="uk-margin">
+                    <a href=""><img class="footer__widthApp" src="images/dang-ky-website-thuong-mai-dien-tu-1024x388-1024x388.png" alt=""></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Liên hệ :</h5>
+                    <ul class="uk-list footer__list">
+                        <li><span class="footer__txt">Email: vnptmedia@vnpt.vn</span></li>
+                        <li><span class="footer__txt">Điện thoại: 02437722728</span></li>
+                        <li><span class="footer__txt">Fax: 02437722733</span></li>
+                    </ul>
+                </div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Địa chỉ :</h5>
+                    <div class="footer__txt">Số 57A phố Huỳnh Thúc Kháng, Phường Láng Hạ, Quận Đống Đa,
+                        Thành phố Hà Nội, Việt Nam.
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <div class="footer__txt">© Copyright 2020 -  Công ty VNPT-MEDIA</div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">ĐKKD:</h5>
+                    <div class="footer__txt">0106873188 do Sở KH-DT cấp ngày 12/06/2015</div>
+                </div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Giấy phép trang tin điện tử số :</h5>
+                    <div class="footer__txt">823/GP-TTĐT do Sở TT-TT Hà Nội cấp ngày 02/03/2017</div>
+                </div>
+                <div class="uk-margin">
+                    <h5 class="uk-h5 uk-text-uppercase footer__title">Tải app vinasport</h5>
+                    <div class="uk-child-width-auto uk-grid-10" uk-grid>
+                        <div><a href=""><img class="footer__widthApp" src="images/adroid@2x.png" alt=""></a></div>
+                        <div><a href=""><img class="footer__widthApp" src="images/ios@2x.png" alt=""></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </section>
 </body>
 </html>
