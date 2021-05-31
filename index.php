d3cd32b25e9aeaa763d8816fa7ecbfd49b261121
Lê Mạnh Hải <hai.lemanh.1310@gmail.com>
2021-05-31 21:48:43 +0700

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         </div>
         <div class="uk-grid-small uk-grid-55-m" uk-grid>
             <div class="uk-width-expand">
-                <div class="uk-grid-small uk-grid-34-m" uk-grid>
+                <div class="uk-grid-collapse home__box2__box" uk-grid>
                     <div class="uk-width-expand">
                         <div class="uk-cover-container home__box1">
                             <img src="images/man-utd-san-co-phat-hien-moi-ma-cu-thay-the-sancho-102650.png" alt="" uk-cover>
@@ -19,41 +19,43 @@
                         </div>
                     </div>
                     <div class="uk-width-1-3@m">
-                        <div class="uk-grid-small uk-grid-20-m" uk-grid>
-                            <?php
-                            $data = array(
-                                array(
-                                    'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/livospwi/2021_05_29/anh_bia.jpg',
-                                    'title' => 'Dàn sao Man City có chỉ số vượt trội Chelsea trong FIFA 21',
-                                ),
-                                array(
-                                    'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/qxjrcqjwq/2021_05_29/joachim_low_germany_2020_1pg4mkzlyvaz81p1ie4duu91kq_1.jpg',
-                                    'title' => 'HLV tuyển Đức lên tiếng về tin dẫn dắt Real Madrid',
-                                ),
-                                array(
-                                    'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/mdf_nsozxd/2021_05_29/2.jpg',
-                                    'title' => 'Man City đang giữ kỷ lục nào của bóng đá Anh ở Champions League?',
-                                ),
-                                array(
-                                    'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/mdf_drokxr/2021_05_29/man_city.jpg',
-                                    'title' => 'Tử huyệt của Chelsea và Man City',
-                                ),
-                            );
-                            foreach ($data as $k => $v): ?>
-                            <div class="uk-width-1-1">
-                                <div class="uk-grid-small home__box2" uk-grid>
-                                    <div class="uk-width-2-5">
-                                        <div class="uk-cover-container home__box2__img">
-                                            <img src="<?= $v['src'] ?>" alt="" uk-cover>
-                                            <canvas width="248" height="178"></canvas>
+                        <div class="uk-padding-small">
+                            <div class="uk-grid-small uk-grid-20-m" uk-grid>
+                                <?php
+                                $data = array(
+                                    array(
+                                        'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/livospwi/2021_05_29/anh_bia.jpg',
+                                        'title' => 'Dàn sao Man City có chỉ số vượt trội Chelsea trong FIFA 21',
+                                    ),
+                                    array(
+                                        'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/qxjrcqjwq/2021_05_29/joachim_low_germany_2020_1pg4mkzlyvaz81p1ie4duu91kq_1.jpg',
+                                        'title' => 'HLV tuyển Đức lên tiếng về tin dẫn dắt Real Madrid',
+                                    ),
+                                    array(
+                                        'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/mdf_nsozxd/2021_05_29/2.jpg',
+                                        'title' => 'Man City đang giữ kỷ lục nào của bóng đá Anh ở Champions League?',
+                                    ),
+                                    array(
+                                        'src' => 'https://znews-photo.zadn.vn/w210/Uploaded/mdf_drokxr/2021_05_29/man_city.jpg',
+                                        'title' => 'Tử huyệt của Chelsea và Man City',
+                                    ),
+                                );
+                                foreach ($data as $k => $v): ?>
+                                    <div class="uk-width-1-1">
+                                        <div class="uk-grid-small home__box2" uk-grid>
+                                            <div class="uk-width-2-5">
+                                                <div class="uk-cover-container home__box2__img">
+                                                    <img src="<?= $v['src'] ?>" alt="" uk-cover>
+                                                    <canvas width="248" height="178"></canvas>
+                                                </div>
+                                            </div>
+                                            <div class="uk-width-expand">
+                                                <h5 class="uk-h5 home__box2__title uk-margin-remove"><a href=""><?= $v['title'] ?></a></h5>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="uk-width-expand">
-                                        <h5 class="uk-h5 home__box2__title uk-margin-remove"><a href=""><?= $v['title'] ?></a></h5>
-                                    </div>
-                                </div>
+                                <?php endforeach; ?>
                             </div>
-                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
@@ -81,7 +83,7 @@
                     );
                     foreach ($data as $k => $v): ?>
                     <div class="uk-width-1-1">
-                        <div class="uk-grid-collapse uk-background-default uk-box-shadow-medium uk-grid-match" uk-grid>
+                        <div class="uk-grid-collapse uk-background-default uk-box-shadow-medium uk-grid-match home__box3" uk-grid>
                             <div class="uk-width-1-3@m">
                                 <div class="uk-cover-container">
                                     <img src="<?= $v['src'] ?>" alt="" uk-cover>
